0e2cd3097f3075856695a666f8c92d48a167eaed
fitodanendra <93357854+fitodanendra@users.noreply.github.com>
2023-12-23 10:04:02 +0700

    Added background in tutorial scene
    tried making a parallax effect, kinda worked but not what i expected to be

---
+++ Assets/Scripts/Parallax Background/ParallaxController.cs
@@ -0,0 +1,65 @@
+
+using UnityEngine;
+
+public class ParallaxController : MonoBehaviour
+{
+
+    Transform cam;
+    Vector3 camStartPos;
+    float distance;
+
+    GameObject[] backgrounds;
+    Material[] mat;
+    float[] backspeed;
+
+    float farthestBack;
+
+    [Range(0.01f, 0.05f)]
+    public float parallaxSpeed;
+
+    void Start()
+    {
+        cam = Camera.main.transform;
+        camStartPos = cam.position;
+
+        int backCount = transform.childCount;
+        mat = new Material[backCount];
+        backgrounds = new GameObject[backCount];
+
+        for (int i = 0; i < backCount; i++)
+        {
+            backgrounds[i] = transform.GetChild(i).gameObject;
+            mat[i] = backgrounds[i].GetComponent<Renderer>().material;
+        }
+        BackspeedCalculate(backCount);
+    }
+
+        void BackspeedCalculate(int backCount)
+        {
+            for (int i = 0; i < backCount; i++)
+            {
+                if((backgrounds[i].transform.position.z - cam.position.z)>farthestBack)
+                {
+                    farthestBack = backgrounds[i].transform.position.z - cam.position.z;
+                }
+            }
+
+            for (int i = 0; i < backCount; i++)
+            {
+                backspeed[i] = 1 - (backgrounds[i].transform.position.z - cam.position.z) / farthestBack;
+            }
+        }
+
+    private void LateUpdate()
+    {
+        distance = cam.position.x - camStartPos.x;
+        transform.position = new Vector3(cam.position.x, transform.position.y, 0);
+
+        for (int i = 0; i<backgrounds.Length; i++)
+        {
+            float speed = backspeed[i] = parallaxSpeed;
+            mat[i].SetTextureOffset("_MainTex", new Vector2(distance, 0) * speed);
+        }
+    }
+
+}

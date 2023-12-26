1717f331956d10728ce8d9cef43fc9a1cd419451
mattVK <138114784+mattVK@users.noreply.github.com>
2023-12-27 00:09:02 +0700

    Revert "Revert "enemy sprite, animation, buggy wall detection""
    This reverts commit 31a80b8cffeaab3ebb1ecb4a7d17d7e25a85f6cd.

---
+++ Assets/Scripts/Enemy/Attack.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    Animator myAnimation; 
+    void Start()
+    {
+        myAnimation = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
